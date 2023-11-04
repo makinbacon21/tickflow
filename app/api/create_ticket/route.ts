d74dba438a25b72c59196e1584544053251957bd
Thomas Makin <halorocker89@gmail.com>
2023-11-04 15:29:27 -0400

    api: add create and get routes

---
+++ app/api/create_ticket/route.ts
@@ -0,0 +1,18 @@
+import prisma from '../../../lib/db'
+
+export async function POST(request: Request) {
+    const json = await request.json()
+    await prisma.ticket.create({
+        data: {
+          user_emails: json['user_emails'],
+          agent_emails: json['agent_emails'],
+          body: json['body'],
+          date_created: new Date(),
+          date_modified: new Date()
+        },
+      }).catch(async (e) => {
+        return Response.json({message: "Failure"})
+      })
+
+      return Response.json({message: "Success"})
+}
---
+++ app/api/get_tickets/route.ts
@@ -0,0 +1,7 @@
+import prisma from '../../../lib/db'
+
+export async function GET() {
+    const tickets = await prisma.ticket.findMany()
+
+    return Response.json(tickets)
+}
