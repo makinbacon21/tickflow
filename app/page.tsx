740e1625fc8b7258b5d8d45a037a7afb3dc8c6d9
Thomas Makin <halorocker89@gmail.com>
2023-11-04 22:05:47 -0400

    page: import api method instead
    Fetching from itself is meh.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import TickGrid from '@/components/TickGrid'
-import Image from 'next/image'
+import { GET } from '@/app/api/get_tickets/route'
 
 export default async function Home() {
-    const dynamicData = await fetch(`http://localhost:3000/api/get_tickets`, { cache: 'no-store', method: 'GET' })
+    const dynamicData = await GET()
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
