444359200d7a9790adbe88771905f88cbb933305
Thomas Makin <halorocker89@gmail.com>
2023-12-05 01:22:05 -0500

    more fixes and now it works!

--- daemon/main.js
+++ daemon/main.js
@@ -1,3 +1,8 @@
+/* TickFlow Daemon
+ * The evil mail client
+ *
+ * I MUST be run as root or I can't do stuff
+ */
 import fs from 'fs';
 import Watcher from 'watcher';
 import { simpleParser } from 'mailparser';
@@ -12,54 +17,47 @@ const getCurrent = () => fs.readFileSync(filepath, {
     encoding: 'ascii'
 })
 
+// clear inbox
 fs.truncate(filepath, 0, function () { console.log('done') })
 
 watcher.on('change', async (event) => {
     console.log("event: " + event);
     let curFile = getCurrent()
+
+    // clear inbox
     fs.truncate(filepath, 0, function () { console.log('done') })
     let out = {}
 
-    if(!curFile) {
-      console.log("empty file!")
-      return
+    if (!curFile) {
+        console.log("empty file!")
+        return
     }
 
-    simpleParser(curFile)
-        .then((parsed) => {
-            if(!parsed || parsed === {}) {
-              console.log("nothing parsed!")
-              return
-            }
-            console.log("parsed:")
-            console.log(parsed)
-            out = {
-                subject: parsed.subject,
-                body: parsed.text,
-                date: parsed.date,
-                agent_emails: parsed.to.text,
-                user_emails: parsed.from.text
-            }
-            if (logfile && logfile != "stdout")
-                fs.writeFileSync(logfile, out)
-            else
-                console.log(out)
-        })
-        .catch((err) => {
-            let errorString = "Failed to parse email! " + err
-            if (logfile && logfile != "stdout")
-                fs.writeFileSync(logfile, errorString)
-            else
-                console.log(errorString)
-        });
+    const parsed = await simpleParser(curFile)
+    if (!parsed || parsed == {}) {
+        console.log("nothing parsed!")
+        return
+    }
 
-   if(!out || out === {}) {
-      console.log("empty!")
-      return
-   }
+    out = {
+        subject: parsed.subject,
+        body: parsed.text,
+        date: parsed.date,
+        agent_emails: parsed.to.text,
+        user_emails: parsed.from.text
+    }
 
-   console.log(out)
+    if (logfile && logfile != "stdout")
+        fs.writeFileSync(logfile, out)
+    else
+        console.log(out)
 
+    if (!out || out == {}) {
+        console.log("empty!")
+        return
+    }
+
+    // post ticket
     await fetch("http://localhost:3000/api/create_ticket", {
         method: "POST",
         body: JSON.stringify(out),
@@ -67,10 +65,11 @@ watcher.on('change', async (event) => {
             "Content-type": "application/json; charset=UTF-8"
         }
     }).then((response) => response.json())
-      .then((json) => console.log(json))
+        .then((json) => console.log(json))
 
 });
 
+// exit handler to kill watcher
 process.on('exit', function () {
     console.log('About to exit.');
     watcher.close();
