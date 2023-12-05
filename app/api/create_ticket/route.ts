591ebffb1d1734c4cc6af711abb1fe21b4b8f546
lsafd <lsafd@tutanota.com>
2023-12-04 20:07:10 -0500

    Template email send on ticket creation

--- app/api/create_ticket/route.ts
+++ app/api/create_ticket/route.ts
@@ -1,4 +1,15 @@
 import prisma from '../../../lib/db'
+var nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.sccs.swarthmore.edu",
+  port: 587,
+  auth: {
+    // TODO: replace `user` and `pass` values
+    user: "REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM",
+    pass: "REPLACE-WITH-YOUR-GENERATED-PASSWORD",
+  },
+});
 
 export async function POST(request: Request) {
     const json = await request.json()
@@ -21,6 +32,9 @@ export async function POST(request: Request) {
       }).catch(async (e) => {
         return Response.json({message: "Failure"})
       })
+
+
+      
 
       return Response.json({message: "Success", id: id,
                     date_created: date_created, date_modified: date_modified})
